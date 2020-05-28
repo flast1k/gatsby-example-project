6d69213a58ed76a8c57bfd54de142a7685bb333f
flast1k <flast1k@gmail.com>
2020-05-28 21:03:47 +0300

    add css module

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'gatsby';
+
 import Layout from '../components/Layout';
 
 export default function Home() {
   return (
     <Layout>
       Hello world!<Link to="/blog/">blog page</Link>
+      <h1>hello styling</h1>
     </Layout>
   );
 }
