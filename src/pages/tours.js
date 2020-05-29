16888a665b07632f02eda54c8ca084f5a04ec4d6
flast1k <flast1k@gmail.com>
2020-05-29 20:02:53 +0300

    add title styled component

--- src/pages/tours.js
+++ src/pages/tours.js
@@ -1,18 +1,9 @@
 import React from 'react';
 
 import Layout from '../components/Layout';
-import Button from '../examples/Button';
 
 const Tours = () => {
-  return (
-    <Layout>
-      Hello from tours page
-      <div>
-        <Button big>first button</Button>
-        <Button color="#f15025">second button</Button>
-      </div>
-    </Layout>
-  );
+  return <Layout>Hello from tours page</Layout>;
 };
 
 export default Tours;
