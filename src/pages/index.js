6a8e2393c35df8cb5342562ab90e8a982deea21b
flast1k <flast1k@gmail.com>
2020-05-29 18:57:17 +0300

    create banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 import Layout from '../components/Layout';
 import SimpleHero from '../components/SimpleHero';
+import Banner from '../components/Banner';
 
 export default function Home() {
   return (
     <Layout>
-      <SimpleHero />
+      <SimpleHero>
+        <Banner
+          title="continue exploring"
+          info="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora, ut."
+        >
+          <Link to="/tours" className="btn-white">
+            explore tours
+          </Link>
+        </Banner>
+      </SimpleHero>
     </Layout>
   );
 }
