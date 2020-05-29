6be63e9a2c9dfe7a584cc85aa4d2d89865fd4c30
flast1k <flast1k@gmail.com>
2020-05-29 18:40:05 +0300

    create main image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 import Layout from '../components/Layout';
+import SimpleHero from '../components/SimpleHero';
 
 export default function Home() {
-  return <Layout>Hello from backroads</Layout>;
+  return (
+    <Layout>
+      <SimpleHero />
+    </Layout>
+  );
 }
