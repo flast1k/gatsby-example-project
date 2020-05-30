ec6f96ec765a4be35001e2ba6c13dbf6ce8037b7
flast1k <flast1k@gmail.com>
2020-05-30 17:22:46 +0300

    add PageQuery

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import { graphql } from 'gatsby';
 
 import Layout from '../components/Layout';
 
-const Blog = () => {
+const Blog = ({ data }) => {
   return (
     <Layout>
       Hello from blog page<Link to="/">back home</Link>
+      <h1>title: {data.site.siteMetadata.title} </h1>
     </Layout>
   );
 };
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+        description
+        author
+        data {
+          age
+        }
+      }
+    }
+  }
+`;
 
 export default Blog;
--- src/pages/tours.js
+++ src/pages/tours.js
@@ -1,14 +1,9 @@
 import React from 'react';
 
 import Layout from '../components/Layout';
-import Header from '../examples/RegularHeader';
 
 const Tours = () => {
-  return (
-    <Layout>
-      <Header />
-    </Layout>
-  );
+  return <Layout />;
 };
 
 export default Tours;
