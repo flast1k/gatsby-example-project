cc9edc30e20807212144933939b087de55bb9c88
flast1k <flast1k@gmail.com>
2020-05-30 19:28:52 +0300

    fix about image

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,14 +2,12 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 import Layout from '../components/Layout';
-import Images from '../examples/Images';
 
 const Blog = () => {
   return (
     <Layout>
       Hello from blog page
       <Link to="/">back home</Link>
-      <Images />
     </Layout>
   );
 };
