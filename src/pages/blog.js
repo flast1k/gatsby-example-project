1051466bac19d3b74857244a67494a9289677d92
flast1k <flast1k@gmail.com>
2020-05-30 18:50:59 +0300

    add gatsby-image example

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 import Layout from '../components/Layout';
+import Images from '../examples/Images';
 
 const Blog = () => {
   return (
     <Layout>
-      Hello from blog page<Link to="/">back home</Link>
+      Hello from blog page
+      <Link to="/">back home</Link>
+      <Images />
     </Layout>
   );
 };
