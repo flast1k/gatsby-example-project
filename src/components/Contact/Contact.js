9d07d327bf63b5fcd7fe9769828a7d585ad3f628
flast1k <flast1k@gmail.com>
2020-05-30 22:43:40 +0300

    add formspree

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -8,7 +8,11 @@ const Contact = () => {
     <section className={styles.contact}>
       <Title title="contact" subtitle="us" />
       <div className={styles.center}>
-        <form className={styles.form}>
+        <form
+          action="https://formspree.io/oopsz0r@gmail.com"
+          method="POST"
+          className={styles.form}
+        >
           <div>
             <label htmlFor="name">name</label>
             <input
