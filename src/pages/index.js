215ef8b3ea1c7361c7949e2f373c7ce7eb9c6a6e
flast1k <flast1k@gmail.com>
2020-05-30 19:56:56 +0300

    setup home page background

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { graphql, Link } from 'gatsby';
 
 import Layout from '../components/Layout';
-import SimpleHero from '../components/SimpleHero';
 import Banner from '../components/Banner';
 import About from '../components/Home/About';
 import Services from '../components/Home/Services';
+import StyledHero from '../components/StyledHero';
 
-export default function Home() {
+export default function Home({ data }) {
+  console.log(data);
+
   return (
     <Layout>
-      <SimpleHero>
+      <StyledHero home="true" img={data.defaultBcg.childImageSharp.fluid}>
         <Banner
           title="continue exploring"
           info="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora, ut."
@@ -19,9 +21,21 @@ export default function Home() {
             explore tours
           </Link>
         </Banner>
-      </SimpleHero>
+      </StyledHero>
       <About />
       <Services />
     </Layout>
   );
 }
+
+export const query = graphql`
+  query {
+    defaultBcg: file(relativePath: { eq: "defaultBcg.jpeg" }) {
+      childImageSharp {
+        fluid(quality: 90, maxWidth: 4160) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`;
