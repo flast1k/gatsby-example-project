8256740004ad576b632800b4b8bf2ab29a469d03
flast1k <flast1k@gmail.com>
2020-05-31 16:31:56 +0300

    add possibility to create pages programmatically

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 exports.onCreateWebpackConfig = ({ getConfig, stage }) => {
   const config = getConfig();
   if (stage.startsWith('develop') && config.resolve) {
@@ -6,4 +8,31 @@ exports.onCreateWebpackConfig = ({ getConfig, stage }) => {
       'react-dom': '@hot-loader/react-dom',
     };
   }
+};
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+
+  const { data } = await graphql(`
+    query {
+      tours: allContentfulTour {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
+    }
+  `);
+
+  data.tours.edges.forEach(({ node }) => {
+    const { slug } = node;
+    createPage({
+      path: `tours/${slug}`,
+      component: path.resolve('./src/templates/tour-template.js'),
+      context: {
+        slug,
+      },
+    });
+  });
 };
