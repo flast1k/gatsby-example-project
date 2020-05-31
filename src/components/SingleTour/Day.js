f171651199e20b828d22df77f52f3d686175aaaf
flast1k <flast1k@gmail.com>
2020-05-31 19:33:35 +0300

    implement day component

--- src/components/SingleTour/Day.js
+++ src/components/SingleTour/Day.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaAngleDown, FaAngleUp } from 'react-icons/fa';
 
-const Day = () => {
-  return <div>this is day 1</div>;
+import styles from '../../css/day.module.css';
+
+const Day = ({ day, info }) => {
+  const [showInfo, setShowInfo] = useState(false);
+  const toggleInfo = () => {
+    setShowInfo(showInfo => !showInfo);
+  };
+
+  return (
+    <article className={styles.day}>
+      <h4>
+        {day}
+        <button className={styles.btn} onClick={toggleInfo}>
+          {showInfo ? <FaAngleUp /> : <FaAngleDown />}
+        </button>
+      </h4>
+      {showInfo && <p>{info}</p>}
+    </article>
+  );
 };
 
 export default Day;
