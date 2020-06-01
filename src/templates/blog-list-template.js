bfb05765c031d4d09c5a827bdb626c06335b27c5
flast1k <flast1k@gmail.com>
2020-06-01 19:18:50 +0300

    add blogs template

---
+++ src/templates/blog-list-template.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import AniLink from 'gatsby-plugin-transition-link/AniLink';
+
+import Layout from '../components/Layout';
+import BlogCard from '../components/Blog/BlogCard';
+import Title from '../components/Title';
+
+import styles from '../css/blog.module.css';
+
+const BlogListTemplate = () => {
+  return <div>this is blogs template</div>;
+};
+
+export default BlogListTemplate;
