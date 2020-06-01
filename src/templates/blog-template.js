4b6aa3570eb02d0aff0d7882df192265491a24ec
flast1k <flast1k@gmail.com>
2020-06-01 18:51:49 +0300

    render images in rich text field

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -13,6 +13,19 @@ const BlogTemplate = ({ data }) => {
     published,
     text: { json },
   } = data.post;
+  const options = {
+    renderNode: {
+      'embedded-asset-block': node => {
+        return (
+          <div className="rich">
+            <h3>this is awesome image</h3>
+            <img width="400" src={node.data.target.fields.file['en-US'].url} />
+            <p>images provided by john doe</p>
+          </div>
+        );
+      },
+    },
+  };
 
   return (
     <Layout>
@@ -21,7 +34,7 @@ const BlogTemplate = ({ data }) => {
           <h1>{title}</h1>
           <h4>published at: {published}</h4>
           <article className={styles.post}>
-            {documentToReactComponents(json)}
+            {documentToReactComponents(json, options)}
           </article>
           <AniLink fade to="/blog" className="btn-primary">
             all posts
