5cbd59702df49ee5d1538c7c8fffa42841a8a06f
flast1k <flast1k@gmail.com>
2020-06-01 18:35:04 +0300

    setup blog template

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import { graphql } from 'gatsby';
+import AniLink from 'gatsby-plugin-transition-link/AniLink';
 
-const BlogTemplate = () => {
-  return <div>this is single blog page</div>;
+import Layout from '../components/Layout';
+
+import styles from '../css/single-blog.module.css';
+
+const BlogTemplate = ({ data }) => {
+  const {
+    title,
+    published,
+    text: { json },
+  } = data.post;
+
+  return (
+    <Layout>
+      <section className={styles.blog}>
+        <div className={styles.center}>
+          <h1>{title}</h1>
+
+          <AniLink fade to="/blog" className="btn-primary">
+            all posts
+          </AniLink>
+        </div>
+      </section>
+    </Layout>
+  );
 };
+
+export const query = graphql`
+  query getPost($slug: String!) {
+    post: contentfulPost(slug: { eq: $slug }) {
+      title
+      published(formatString: "MMMM Do, YYYY")
+      text {
+        json
+      }
+    }
+  }
+`;
 
 export default BlogTemplate;
