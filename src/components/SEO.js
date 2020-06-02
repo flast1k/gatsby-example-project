47416c53d58aa0cd0de6fb69be1e7da4bc591718
flast1k <flast1k@gmail.com>
2020-06-02 19:42:45 +0300

    fix twitter description

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -37,7 +37,7 @@ const Seo = ({ title, description }) => {
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content={twitterUsername} />
       <meta name="twitter:title" content={siteTitle} />
-      <meta name="twitter:description" content={description} />
+      <meta name="twitter:description" content={siteDesc} />
       <meta name="twitter:image" content={`${siteUrl}${image}`} />
     </Helmet>
   );
